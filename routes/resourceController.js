8dec329ede7a198853c920fff2905e697fe9c73f
Charline <charline.henneron@viacesi.fr>
2021-02-12 22:52:06 +0100

    Service ressource détail - Gestion des erreurs

--- routes/resourceController.js
+++ routes/resourceController.js
@@ -54,43 +54,60 @@ module.exports = {
                 WHERE r.active = 1 AND r.id = ` + req.params.resource_id
             config.connexion.query(sql, 
                 (error, result) => {
+                    if (result[0] != undefined) {
 
-                    if (result[0].public != 1) {
-                        const token = req.headers.authorization
-                        decodedToken = jwtUtils.checkToken(token)
-                        sql = `SELECT role_id as roleId FROM user WHERE id = ` + decodedToken.userId
+                        if (result[0].public != 1) {
+                            const token = req.headers.authorization
+                            decodedToken = jwtUtils.checkToken(token)
 
-                        config.connexion.query(sql, 
-                            (error2, result2) => {
-                                if (result2[0].roleId != 1) { 
-                                    //user = modérateur ou admin
-                                    console.log(result2[0].roleId)
-                                    return res.status(200).json(result)
-                                } else {
-                                    sql = `SELECT id FROM rel_user_action_resource 
-                                    WHERE action_type_id = 1 
-                                    AND user_id = ` + decodedToken.userId + ` 
-                                    AND resource_id = ` + req.params.resource_id + ` 
-                                    UNION 
-                                    SELECT id FROM rel_shared_resource_user 
-                                    WHERE shared_with_user_id = ` + decodedToken.userId + ` 
-                                    AND resource_id = ` + req.params.resource_id 
-                                    console.log (sql)
-                                    config.connexion.query(sql, 
-                                        (error3, result3) => {
-                                            if(result3[0] === undefined) {
-                                                return res.status(403).json({"message": "Accès non autorisé - Ressource privée"})
-                                            } else {
-                                                //l'utilisateur est auteur de la ressource ou la ressource a été partagée avec lui
+                            if (decodedToken.error === undefined) {
+
+                                sql = `SELECT role_id as roleId FROM user WHERE id = ` + decodedToken.userId
+
+                                config.connexion.query(sql, 
+                                    (error2, result2) => {
+
+                                        if (result2[0] != undefined) {
+
+                                            if (result2[0].roleId != 1) { 
                                                 return res.status(200).json(result)
+                                            } else {
+                                                sql = `SELECT id FROM rel_user_action_resource 
+                                                WHERE action_type_id = 1 
+                                                AND user_id = ` + decodedToken.userId + ` 
+                                                AND resource_id = ` + req.params.resource_id + ` 
+                                                UNION 
+                                                SELECT id FROM rel_shared_resource_user 
+                                                WHERE shared_with_user_id = ` + decodedToken.userId + ` 
+                                                AND resource_id = ` + req.params.resource_id 
+                                                config.connexion.query(sql, 
+                                                    (error3, result3) => {
+                                                        if(result3[0] === undefined) {
+                                                            return res.status(403).json({"message": "Accès non autorisé - Ressource privée"})
+                                                        } else {
+                                                            //l'utilisateur est auteur de la ressource ou la ressource a été partagée avec lui
+                                                            return res.status(200).json(result)
+                                                        }
+                                                    }
+                                                )
                                             }
+                                            
+                                        } else {
+                                            return res.status(500).json({"message": "Erreur fatale"})
                                         }
-                                    )
-                                }
+                                    }
+                                    
+                                )
+
+                            } else {
+                                return res.status(403).json(decodedToken)
                             }
-                        )
+                        } else {
+                            return res.status(200).json(result)
+                        }
+
                     } else {
-                        return res.status(200).json(result)
+                        return res.status(404).json({"message": "Ressource non trouvée - La ressource est inexistante ou inactive"})
                     }
                     
                 }
